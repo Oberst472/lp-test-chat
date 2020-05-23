f8f010c93cd58cf9ace9bfa1631d18afddde26f9
Oberst47 <oberst47@gmail.com>
2020-05-23 13:43:42 +0300

    style: layout base css

--- src/layouts/main/index.vue
+++ src/layouts/main/index.vue
@@ -1,11 +1,13 @@
 <template>
     <section class="layout-main">
-        <aside class="layout-main__aside">
-            <router-view name="nav" class="layout-main__aside-content"/>
-        </aside>
-        <main class="layout-main__body">
-            <router-view class="layout-main__body-content"/>
-        </main>
+        <div class="layout-main__content">
+            <aside class="layout-main__aside">
+                <router-view name="nav" class="layout-main__aside-content"/>
+            </aside>
+            <main class="layout-main__body">
+                <router-view class="layout-main__body-content"/>
+            </main>
+        </div>
     </section>
 </template>
 
@@ -18,7 +20,20 @@ export default {
 <style lang="scss">
 .layout {
     &-main {
+        display: flex;
         width: 100%;
+        &__content {
+            width: 100%;
+            display: flex;
+        }
+        &__aside {
+            background-color: $color--bg-nav;
+            width: 300px;
+        }
+        &__body {
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+            flex-grow: 1;
+        }
     }
 }
 </style>
