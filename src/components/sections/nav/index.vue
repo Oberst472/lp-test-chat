3a168253b93818693a77c868c97ae4110434936e
Oberst47 <oberst47@gmail.com>
2020-05-23 14:51:11 +0300

    style: aside custom scrollbar

--- src/components/sections/nav/index.vue
+++ src/components/sections/nav/index.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="section-aside">
         <div class="section-aside__content">
-            <div class="section-aside__stat">Сообщений: 151</div>
+            <div class="section-aside__stat">Диалоги <span class="section-aside__stat-count">154</span></div>
             <div class="section-aside__items">
-                <BlockNavShortItem v-for="item in 20" :key="item"/>
+                <VueScroll>
+                    <BlockNavShortItem v-for="item in 3" :key="item"/>
+                </VueScroll>
             </div>
             <div class="section-aside__add-chat">Добавить новый чат</div>
         </div>
@@ -11,37 +13,49 @@
 </template>
 
 <script>
+    import VueScroll from 'vuescroll';
     import BlockNavShortItem from '@/components/blocks/navShortItem'
-export default {
-    components: {
-        BlockNavShortItem
+
+    export default {
+        components: {
+            VueScroll,
+            BlockNavShortItem
+        }
     }
-}
 </script>
 
 <style lang="scss" scoped>
-.section-aside {
-    &__content {
+    .section-aside {
+        &__content {}
 
-    }
-    &__stat {
-        height: 60px;
-        border-bottom: 1px solid #E9EDF2;
-        display: flex;
-        align-items: center;
-        padding: 22px 20px;
-        box-sizing: border-box;
+        &__stat {
+            height: 60px;
+            border-bottom: 1px solid #E9EDF2;
+            display: flex;
+            align-items: center;
+            padding: 22px 20px;
+            box-sizing: border-box;
+            font-size: 14px;
+            line-height: 20px;
+            color: $color--text-dark;
 
+            &-count {
+                color: $color--text-light;
+                font-weight: $weight--bold;
+                margin-left: 10px;
+            }
+
+        }
+
+        &__items {
+            height: calc(100vh - 120px);
+        }
+
+        &__add-chat {
+            background-color: $color--positive;
+            height: 60px;
+            box-sizing: border-box;
+            padding: 22px 20px;
+        }
     }
-    &__items {
-        height: calc(100vh - 120px);
-        overflow: scroll;
-    }
-    &__add-chat {
-        background-color: $color--positive;
-        height: 60px;
-        box-sizing: border-box;
-        padding: 22px 20px;
-    }
-}
 </style>
